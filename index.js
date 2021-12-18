464011e2c411d2a6048649f6ef776683242c7381
Mani <manikanta222010gmail.com>
2021-12-18 21:39:02 +0530

    final

---
+++ index.js
@@ -0,0 +1,69 @@
+import express from "express"
+
+const app = express()
+
+// sample data of rooms
+const rooms = [
+    {
+        "id": "100",
+        "roomname": "1st room",
+        "seats": 200,
+        "amenities": ["ac", "water"],
+        "priceperhr": 5000,
+        "bookeddetails": {
+            "customername": "mani",
+            "date": "3-12-2021",
+            "starttime": "10am",
+            "endtime": "12pm",
+        }
+    },
+    {
+        "id": "101",
+        "roomname": "2nd room",
+        "seats": 300,
+        "amenities": ["ac", "water"],
+        "priceperhr": 7000,
+    }
+]
+
+const PORT = 8000
+
+app.use(express.json())
+
+app.get("/", (request, response) => {
+    response.send(rooms)
+})
+
+// route to insert data of new room
+app.post("/create-room", (request, response) => {
+    const data = request.body
+    rooms.push(data)
+    response.send(rooms)
+})
+
+// route to book a room with the room id
+app.post("/book-room/:id", (request, response) => {
+    const { id } = request.params
+    const data = request.body
+    const room = rooms.find(x => x.id == id)
+    if (room.bookeddetails) {
+        response.send("Room was already booked")
+    } else {
+        room.bookeddetails = data
+        response.send(rooms)
+    }
+})
+
+// route to list the booked rooms
+app.get("/booked-rooms", (request, response) => {
+    const bookedrooms = rooms.filter(room => room.bookeddetails)
+    console.log(bookedrooms)
+    response.send(bookedrooms)
+})
+
+// route to list all the customers
+app.use("/customers", (request, response) => {
+    response.send(rooms)
+})
+
+app.listen(PORT, () => console.log("App started in ", PORT))
